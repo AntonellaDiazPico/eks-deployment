05c3e34dda67510aee489c8fd31423e771b1c1db
AntonellaDiazPico <antonelladiazpico@gmail.com>
2025-12-01 13:22:09 +0100

    Feat: adds cloudwatch serviceaccount

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -93,3 +93,32 @@ resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller_attach" 
   role       = aws_iam_role.aws_load_balancer_controller.name
   policy_arn = "arn:aws:iam::676206912141:policy/AWSLoadBalancerControllerIAMPolicy"
 }
+
+# IAM role for CloudWatch Agent
+data "aws_iam_policy_document" "cloudwatch_agent_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:default:cloudwatch-agent"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "cloudwatch_agent" {
+  assume_role_policy = data.aws_iam_policy_document.cloudwatch_agent_assume_role_policy.json
+  name               = "CloudWatchAgentRole"
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent_attach" {
+  role       = aws_iam_role.cloudwatch_agent.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,3 +17,8 @@ output "aws_load_balancer_controller_role_arn" {
   description = "ARN of the AWS Load Balancer Controller IAM role"
   value       = aws_iam_role.aws_load_balancer_controller.arn
 }
+
+output "cloudwatch_agent_role_arn" {
+  description = "ARN of the CloudWatch Agent IAM role"
+  value       = aws_iam_role.cloudwatch_agent.arn
+}
